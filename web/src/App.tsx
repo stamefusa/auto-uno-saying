c29b84146435dfb9af5dcfa6d9d026564cbeb6bf
Shintaro Tamefusa <tameshin@gmail.com>
2026-03-07 00:49:42 +0900

    add web template

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <p className="text-2xl font-bold">UNO自動宣言装置</p>
+    </div>
+  )
+}
+
+export default App
